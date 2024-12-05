d17388c08700c896bb7c507085dbcc22c32934fd
Nweaver412 <nw21010@gmail.com>
2024-12-05 09:47:57 -0700

    Trying to fix internal server error

--- src/component.py
+++ src/component.py
@@ -48,21 +48,22 @@ class Component(ComponentBase):
             settings=settings,
         )
         dataset.create()
-        logging.info(f"Dataset {dataset_name} created in Argilla.")
 
         # Get input tables
         input_tables = self.get_input_tables_definitions()
+        
         if len(input_tables) == 0:
             raise UserException("No input tables found")
 
         input_table = input_tables[0]
-        logging.info(f'Processing table: {input_table.name}')
 
         # Read input data
         records = []
+
+        # Opens table
         with open(input_table.full_path, mode='r', encoding='utf-8') as inp_file:
             reader = csv.DictReader(inp_file)
-            for row in reader:
+            for row in reader: # Iterate over rows
                 text = row.get("text", "").strip()
                 messageId = row.get("messageId", "").strip()
                 partId = row.get("partId", "").strip()
@@ -72,23 +73,21 @@ class Component(ComponentBase):
 
                 label = row.get("label", "").strip()
 
-                if text and label:  # Ensure necessary fields are present
-                    record = {
-                        "text": text,
-                        "messageId": messageId,
-                        "partId": partId,
-                        "mimeType": mimeType,
-                        "bodySize": bodySize,
-                        "bodyData": bodyData,
-                        "label": label
-                    }
-                    records.append(record)
+                record = {
+                    "text": text,
+                    "messageId": messageId,
+                    "partId": partId,
+                    "mimeType": mimeType,
+                    "bodySize": bodySize,
+                    "bodyData": bodyData,
+                    "label": label
+                }
+                records.append(record)
 
         # Log records to Argilla
         dataset.records.log(records)
         logging.info(f"{len(records)} records logged to Argilla dataset: {dataset_name}")
 
-        # Write new state
         self.write_state_file({"last_dataset_name": dataset_name})
 
 
